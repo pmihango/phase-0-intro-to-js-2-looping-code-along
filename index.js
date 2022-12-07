6ac13a9c7091c9f904803a441fc7a2ab357c4ae4
pmihango <pmihango@gmail.com>
2022-12-07 12:29:59 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+// Code your solutions in this file
+function writeCards(name){
+    let newArray = [];
+    for(let i = 0; i < name.length; i++){
+        let message = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+let count;
+function countDown(count){
+    while(count >=0){
+        console.log(count);
+        count--;
+    }
+}
+countDown(count)
+
+
+
+
+
+
